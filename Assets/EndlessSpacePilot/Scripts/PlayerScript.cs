b4eb80eb026f3610fa282b29dd91d0c0c750ddb4
Filipe Brenner <filipe.brener@ufv.br>
2023-04-11 01:13:23 -0300

    adição de combate entre o player e o inimigo

--- Assets/EndlessSpacePilot/Scripts/PlayerScript.cs
+++ Assets/EndlessSpacePilot/Scripts/PlayerScript.cs
@@ -5,17 +5,19 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour{
 
     public float speed = 1;
-    public Vector2 screenLimit;
+    public Vector2 screenLimit = new Vector2(11.555F, 5.555F);
     public GameObject projectile;
     public Transform[] shootPositions;
     public Vector2 shootDirection = new(1,0);
     public float shootCD = .5f;
     public float shootSpeed = 300;
     float shootTimer = 0;
+    public int maxLife = 10;
+    public int life;
 
     // Start is called before the first frame update
     void Start(){
-        
+        life = maxLife;
     }
 
     // Update is called once per frame
@@ -25,10 +27,25 @@ public class PlayerScript : MonoBehaviour{
         Shoot();
     }
 
+    public void TakeDamage(int damage = 1)
+    {
+        if (damage < 0) return;
+        life -= damage;
+        if(life <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        life = maxLife;
+        transform.position = new Vector3(screenLimit.x / 2, screenLimit.y / 2);
+    }
+
     void Shoot(){
         if (Input.GetAxisRaw("Jump") != 0 && shootTimer >= shootCD){
             for(int i = 0; i < shootPositions.Length; i++){
-                print("Atirando com o : " + shootPositions.Length);
                 GameObject shoot = Instantiate(projectile);
                 shoot.transform.position = shootPositions[i].position;
                 shoot.transform.up = shootDirection.normalized;
@@ -42,8 +59,8 @@ public class PlayerScript : MonoBehaviour{
         float hMove = Input.GetAxisRaw("Horizontal");
         float vMove = Input.GetAxisRaw("Vertical");
         transform.Translate(speed * Time.deltaTime * new Vector3(hMove, vMove).normalized);
-        if (transform.position.x > screenLimit.x) transform.position = new Vector3(-screenLimit.x + .2f, transform.position.y);
-        if (transform.position.x < -screenLimit.x) transform.position = new Vector3(screenLimit.x - .2f, transform.position.y);
+        if (transform.position.x > screenLimit.x) transform.position = new Vector3(screenLimit.x, transform.position.y);
+        if (transform.position.x < -screenLimit.x) transform.position = new Vector3(-screenLimit.x, transform.position.y);
         if (transform.position.y > screenLimit.y) transform.position = new Vector3(transform.position.x, -screenLimit.y + .2f);
         if (transform.position.y < -screenLimit.y) transform.position = new Vector3(transform.position.x, screenLimit.y - .2f);
     }
--- Assets/EndlessSpacePilot/Scripts/ProjectileScript.cs
+++ Assets/EndlessSpacePilot/Scripts/ProjectileScript.cs
@@ -20,4 +20,21 @@ public class ProjectileScript : MonoBehaviour{
         yield return new WaitForSeconds(durationInSeconds);
         Destroy(this.gameObject);
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("Sou:" + (CompareTag("EnemyProjectile") ? "EnemyProjectile" : CompareTag("PlayerProjectile") ? "PlayerProjectile" : CompareTag("Player") ? "Player" : CompareTag("Enemy") ? "Enemy" : "Não sei o que sou :C"));
+        print("Colidi em:" +  (collision.CompareTag("EnemyProjectile") ? "EnemyProjectile" : collision.CompareTag("PlayerProjectile") ? "PlayerProjectile" : collision.CompareTag("Player") ? "Player" : collision.CompareTag("Enemy") ? "Enemy" : "Não sei o que sou :C"));
+        if (collision.CompareTag("Player") && CompareTag("EnemyProjectile"))
+        {
+            Destroy(this.gameObject);
+            collision.GetComponent<PlayerScript>().TakeDamage();
+        }
+        if (collision.CompareTag("Enemy") && CompareTag("PlayerProjectile"))
+        {
+            collision.GetComponent<EnemyScript>().TakeDamage();
+            Destroy(this.gameObject);
+        }
+    }
+
 }
